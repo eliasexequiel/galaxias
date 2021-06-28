7680275fb162f072cce29b7d065f5ddca83a6238
Exequiel <eenichele@gmail.com>
2021-06-28 20:52:14 -0300

    Añado parte 2

---
+++ utils.py
@@ -0,0 +1,18 @@
+def remove_outlier(df_in, col_name, iqr_factor):
+    """
+  IQR outliers removal (https://machinelearningmastery.com/how-to-use-statistics-to-identify-outliers-in-data/)
+  accept a dataframe, remove outliers, return cleaned data in a new dataframe
+  see http://www.itl.nist.gov/div898/handbook/prc/section1/prc16.htm
+  """
+    q1 = df_in[col_name].quantile(0.25)
+    q3 = df_in[col_name].quantile(0.75)
+    iqr = q3 - q1  # interquartile range
+    fence_low = q1 - iqr_factor * iqr
+    fence_high = q3 + iqr_factor * iqr
+    df_inliers = df_in.loc[
+        (df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)
+    ]
+    df_outliers = df_in.loc[
+        (df_in[col_name] < fence_low) | (df_in[col_name] > fence_high)
+    ]
+    return df_inliers, df_outliers
